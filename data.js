c541e8a2e6a9663ca667247dda3aaa999c544833
xdsport2288-droid <xdsport2288@gmail.com>
2026-07-11 16:23:30 +0900

    자동 업데이트: data.js 최신 데이터 반영

--- data.js
+++ data.js
@@ -1,4 +1,30 @@
-window.LAST_UPDATED = '2026-07-11 15:48:21';
+window.LAST_UPDATED = '2026-07-11 16:23:27';
+window.DASHBOARD_CONFIG = {
+  "shipper_filter": {
+    "display_name": "화주사",
+    "order": 1
+  },
+  "date_filter": {
+    "display_name": "운송 기간",
+    "order": 2
+  },
+  "status_filter": {
+    "display_name": "접수상태",
+    "order": 3
+  },
+  "loading_filter": {
+    "display_name": "출발지명",
+    "order": 4
+  },
+  "dest_filter": {
+    "display_name": "도착지명",
+    "order": 5
+  },
+  "tone_filter": {
+    "display_name": "⚖️ 차량톤수",
+    "order": 6
+  }
+};
 window.TRANSPORT_DATA = [
   {
     "주문 상태": "배차완료",
