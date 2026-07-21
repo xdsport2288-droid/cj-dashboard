06fb05abf9fd82f0ad81af8bcc4cb87727675d46
xdsport2288-droid <xdsport2288@gmail.com>
2026-07-21 12:40:48 +0900

    자동 업데이트: data.js 최신 데이터 반영

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
-window.LAST_UPDATED = '2026-07-21 12:36:52';
+window.LAST_UPDATED = '2026-07-21 12:40:51';
 window.DASHBOARD_CONFIG = {
   "shipper_filter": {
     "display_name": "화주사",
@@ -52,7 +52,7 @@ window.TRANSPORT_DATA = [
     "경유지": 0.0,
     "수량": 1.0,
     "비고": "일코전자 경기 전자랜드",
-    "간선사": "5대5",
+    "간선사": "5대56",
     "운송사": "",
     "소속": "",
     "추가운임": ""
